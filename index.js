3d4c60f4765c7cb2d14df49fdd808330afc46c1d
Qwertyindividual <brandtkauthar@gmail.com>
2023-02-16 15:45:33 +0200

    commit

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-// var add = require("./addition");
-
-// let sum = add;
-// sum(5,7)
-
-// let {addition} = require("./addition");
-// addition(3,7);
-// addition(7,7);
-
-// const http = require('http');
-
-//port
-
-// const port = parseInt(process.env.port) || 4000;
-
-// http.createServer( (req, res)=>{
-//     const currUrl = req.url;
-//     console.log('Url: ', currUrl, '\nMethod: ', req.method);
-//     res.writeHead(200, {'Content-type': 'text/html'});
-//     switch(currUrl) {
-//         case '/':
-//             res.end('You are home');
-//         break
-//         case '/about':
-//             res.end('About me page');
-//         break
-//         case '/data':
-//             res.end('Page data');
-//         break
-//         default:
-//             res.end('Page / content was not found');
-//     }
-// }). listen(port, ()=>{
-//     console.log(`Server is running at port ${port}`);
-// })
 
 const express = require('express');
 //Express app
@@ -43,9 +8,7 @@ const db = require('./config');
 
 const bodyParser = require('body-parser'); 
 
-// const jsonParser = bodyParser.json();
 
-// const urlencodedParser = bodyParser.urlencoded({extended: false})
 
 
 const port = parseInt(process.env.port) || 4000;
